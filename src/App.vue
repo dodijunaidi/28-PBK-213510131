0fbd611cb7b7913f6f2a76c7703c34cad38e1a24
dodijunaidi <130949221+dodijunaidi@users.noreply.github.com>
2023-04-17 10:03:05 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="todo-list-container">
     <h1>TO-DO LIST</h1>
     <form @submit.prevent="addActivity">
       <input type="text" v-model="newActivity" placeholder="Add activity...">
@@ -9,7 +9,7 @@
       <li v-for="(activity, index) in filteredActivities" :key="index" :class="{ done: activity.completed }">
         <input type="checkbox" v-model="activity.completed">
         <span>{{ activity.text }}</span>
-        <button @click="removeActivity(index)">Remove</button>
+        <button class="remove-button" @click="removeActivity(index)">Remove</button>
       </li>
     </ul>
     <div>
@@ -55,8 +55,73 @@ export default {
 </script>
 
 <style>
-.done {
-  text-decoration: line-through;
+.todo-list-container {
+  max-width: 500px;
+  margin: auto;
+  padding: 20px;
+  background-color: #f5f5f5;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+  border-radius: 5px;
+}
+
+h1 {
+  text-align: center;
+  color: #4a4a4a;
+}
+
+form {
+  display: flex;
+  margin-bottom: 20px;
+}
+
+form input[type="text"] {
+  flex: 1;
+  padding: 10px;
+  font-size: 16px;
+  border-radius: 5px 0 0 5px;
+  border: none;
+}
+
+form button {
+  padding: 10px;
+  font-size: 16px;
+  border: none;
+  border-radius: 0 5px 5px 0;
+  background-color: #2ecc71;
+  color: #fff;
+  cursor: pointer;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+li {
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+}
+
+li input[type="checkbox"] {
+  margin-right: 10px;
+  cursor: pointer;
+}
+
+li span {
+  flex: 1;
+  color: #4a4a4a;
+  font-size: 16px;
+}
+
+li button.remove-button {
+  background-color: #e74c3c;
+  color: #fff;
+  border: none;
+  padding: 5px 10px;
+  border-radius: 5px;
+  margin-left: 10px;
+  cursor: pointer;
 }
 </style>
-
